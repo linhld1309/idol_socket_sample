b6ce04d2c6888e90138f897940ddfded1347bdfd
linhld <zero.silvermoonlight@gmail.com>
2022-10-26 18:21:03 +0900

    update ioClient

--- index.js
+++ index.js
@@ -71,10 +71,14 @@ server.listen(port, () => {
 });
 
 const io = require("socket.io-client");
-const ioClient = io.connect(`https://idol.gotechjsc.com:${port}`, {
-    secure: true,
-    reconnection: true,
-    rejectUnauthorized: false
+const ioClient = io(`https://idol.gotechjsc.com:${port}`, {
+  key: readFileSync("/home/ubuntu/apps/myapp/shared/ssl/socket_key.pem"),
+  cert: readFileSync("/home/ubuntu/apps/myapp/shared/ssl/socket_cert.pem"),
+  ca: [
+    readFileSync("/home/ubuntu/socket_io/ssl/fullchain.pem")
+  ],
+  secure: true,
+  reconnection: true,
+  rejectUnauthorized: false
 });
-
 ioClient.on(EVENTS.CONVERSATION_SEND, (msg) => console.info('Simulated client: ' + msg));
